b2c4c5ffd226f456346720cf43f96f2ccac4971b
RicardoPang <ricardo.pangj@gmail.com>
2025-06-30 10:30:37 +0800

    版本发布

--- packages/hooks/index.js
+++ packages/hooks/index.js
@@ -1,3 +1,3 @@
 const init = require('@pf/ui');
-console.log("🍌 PF 🍌")
+console.log("🍌PF")
 init();
--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,4 +1,4 @@
 function init() {
-  console.log('---UI初始化🍌🍌---');
+  console.log('---UI初始化🍌---');
 }
 module.exports = init;
